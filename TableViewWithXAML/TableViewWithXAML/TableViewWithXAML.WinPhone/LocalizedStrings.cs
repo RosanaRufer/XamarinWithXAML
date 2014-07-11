985c302aa7a2ead7007a38f1cdbb3d013bee5195
Rosana Ruiz <rosanarufer@gmail.com>
2014-07-11 11:41:44 +0200

    wip

---
+++ TableViewWithXAML/TableViewWithXAML/TableViewWithXAML.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using TableViewWithXAML.WinPhone.Resources;
+
+namespace TableViewWithXAML.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ TableViewWithXAML/TableViewWithXAML/TableViewWithXAML.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using Xamarin.Forms;
+
+
+namespace TableViewWithXAML.WinPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            Content = TableViewWithXAML.App.GetMainPage().ConvertPageToUIElement(this);
+        }
+    }
+}
